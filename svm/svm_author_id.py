797526eeab4301d434c1691bb49f31c52a9d5d29
Gordon Chiam <gordon.chiam@gmail.com>
2017-03-12 15:42:54 +0800

    Solve Quiz: SVM Author ID Accuracy

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -19,12 +19,18 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
 
+from sklearn.svm import SVC
+
+classifier = SVC(kernel='linear')
+
+start_time = time()
+classifier.fit(features_train, labels_train)
+print 'Training time:', time() - start_time
+
+score = classifier.score(features_test, labels_test)
+print 'Score:', score
+
 #########################################################
-
-
